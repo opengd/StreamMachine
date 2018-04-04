f92ca02a664df9414a3ba3d22e0ec44be9f9de9c
Erik Johansson <opengd@outlook.com>
2018-04-04 23:25:58 +0200

    Add BA project and WRC project

--- StreamMachine/MainPage.xaml.cs
+++ StreamMachine/MainPage.xaml.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Audio;
+using Windows.Media.Core;
+using Windows.Media.Devices;
+using Windows.Media.Playback;
+using Windows.Media.Render;
+using Windows.Networking.Sockets;
+using Windows.Storage;
+using Windows.Storage.BulkAccess;
+using Windows.Storage.Search;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +26,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +37,293 @@ namespace StreamMachine
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private AudioGraph graph;
+        private AudioFileInputNode fileInput;
+        private AudioDeviceOutputNode deviceOutput;
+
+        private MediaPlayer mediaPlayer;
+
+        IReadOnlyList<FileInformation> fil;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            //await ScanFiles();
+
+            await CreateMediaPlayer();
+
+            //PlayMusic();
+
+            //await CreateAudioGraph();
+            //await PlayMyMusic();
+
+            var listener = new StreamSocketListener();
+            await listener.BindServiceNameAsync("8000");
+            listener.ConnectionReceived += Listener_ConnectionReceived;
+
+            //
+
+            await AzureIoTHub.RegisterDirectMethodsAsync();
+
+
+
+            //ListenToIot();
+        }
+
+        private async Task ListenToIot()
+        {
+            string msg = string.Empty;
+
+            while (true)
+            {
+                try
+                {
+                    msg = await AzureIoTHub.ReceiveCloudToDeviceMessageAsync();
+                    //await AzureIoTHub.SendDeviceToCloudMessageAsync();
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+
+                if (msg.Contains("play"))
+                {
+                    PlayMusic();
+                }
+                else if (msg.Contains("stop"))
+                {
+                    StopMusic();
+                }
+
+                Debug.WriteLine(msg);
+            }
+        }
+
+        private async void Listener_ConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            StringBuilder request = new StringBuilder();
+
+            using (IInputStream input = args.Socket.InputStream)
+            {
+                byte[] data = new byte[8192];
+                IBuffer buffer = data.AsBuffer();
+                uint dataRead = 8192;
+                while (dataRead == 8192)
+                {
+                    await input.ReadAsync(buffer, 8192, InputStreamOptions.Partial);
+                    request.Append(Encoding.UTF8.GetString(data, 0, data.Length));
+                    dataRead = buffer.Length;
+                }
+
+
+            }
+
+            var req = request.ToString();
+
+            if (req.ToLower().Contains("play"))
+            {
+                PlayMusic();
+            }
+            else if (req.ToLower().Contains("stop"))
+            {
+                StopMusic();
+            }
+
+            Debug.WriteLine(request.ToString());
+
+            string header = string.Format("HTTP/1.1 200 OK\r\n" +
+                              "Content-Type: text/html\r\n" +
+                              "Date: " + DateTime.Now.ToString("R") + "\r\n" +
+                              "Server: IotPlayMusic/1.0\r\n" +
+                              "Content-Length: 2\r\n" +
+                              "Connection: close\r\n\r\n" +
+                              "OK");
+
+            Debug.WriteLine(header);
+            //var buf = Encoding.UTF8.GetBytes("OK").AsBuffer();
+
+            var buf = Encoding.UTF8.GetBytes(header).AsBuffer();
+
+            using (IOutputStream output = args.Socket.OutputStream)
+            {
+                await output.WriteAsync(buf);
+
+                await output.FlushAsync();
+            }
+
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            // Destroy the graph if the page is naviated away from
+
+            if (graph != null)
+            {
+                graph.Dispose();
+            }
+        }
+
+        private void MediaPlayerElement_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Uri pathUri = new Uri("ms-appx:///video-1448567804.mp4");
+                //mediaPlayer.Source = MediaSource.CreateFromUri(pathUri);
+            }
+            catch (Exception ex)
+            {
+                if (ex is FormatException)
+                {
+                    // handle exception.
+                    // For example: Log error or notify user problem with file
+                }
+            }
+
+        }
+
+        private async Task CreateMediaPlayer()
+        {
+            mediaPlayer = new MediaPlayer();
+
+            mediaPlayer.AudioCategory = MediaPlayerAudioCategory.Media;
+
+            var outputDevices = await DeviceInformation.FindAllAsync(MediaDevice.GetAudioRenderSelector());
+
+            foreach (var device in outputDevices)
+            {
+                Debug.WriteLine(device.Name);
+
+                if (device.Name.Contains("Halide"))
+                {
+                    mediaPlayer.AudioDevice = device;
+                }
+            }
+        }
+
+        private void PlayMusic()
+        {
+            //var file = await StorageFile.GetFileFromPathAsync(fil[0].Path);
+
+            //mediaPlayer.Source = MediaSource.CreateFromStorageFile(file);
+
+            var uri = new Uri(@"http://http-live.sr.se/p2musik-mp3-192");
+            //var uri = new Uri(@"http://sverigesradio.se/topsy/direkt/2562-hi-mp3.m3u");
+
+            mediaPlayer.Source = MediaSource.CreateFromUri(uri);
+
+            mediaPlayer.Play();
+        }
+
+        private void StopMusic()
+        {
+            mediaPlayer.Pause();
+            mediaPlayer.Source = null;
+        }
+
+        private async Task ScanFiles()
+        {
+
+            var fileTypeFilter = new List<string>
+            {
+                ".mp3",
+                ".flac"
+            };
+            var queryOptions = new QueryOptions(CommonFileQuery.OrderByName, fileTypeFilter)
+            {
+                FolderDepth = FolderDepth.Deep,
+                IndexerOption = IndexerOption.DoNotUseIndexer
+            };
+
+            // Create query and retrieve files
+            var query = KnownFolders.MusicLibrary.CreateFileQueryWithOptions(queryOptions);
+            //IReadOnlyList<StorageFile> fileList = await query.GetFilesAsync();
+
+            var fif = new FileInformationFactory(query, Windows.Storage.FileProperties.ThumbnailMode.MusicView);
+
+            fil = await fif.GetFilesAsync();
+
+            Debug.WriteLine("Count: " + fil.Count);
+
+            foreach (var fi in fil)
+            {
+                Debug.WriteLine(fi.Path);
+
+            }
+        }
+
+        private async Task PlayMyMusic()
+        {
+            var file = await StorageFile.GetFileFromPathAsync(fil[0].Path);
+            //var file = await KnownFolders.MusicLibrary.GetFileAsync("smalltest.flac");
+
+            CreateAudioFileInputNodeResult fileInputResult = await graph.CreateFileInputNodeAsync(file);
+
+            if (fileInputResult.Status != AudioFileNodeCreationStatus.Success)
+            {
+                // Cannot create graph
+                //rootPage.NotifyUser(String.Format("AudioGraph Creation Error because {0}", result.Status.ToString()), NotifyType.ErrorMessage);
+                return;
+            }
+
+            fileInput = fileInputResult.FileInputNode;
+            fileInput.AddOutgoingConnection(deviceOutput);
+            //fileInput.LoopCount = 10;
+            graph.Start();
+        }
+
+        private async Task CreateAudioGraph()
+        {
+            // Create an AudioGraph with default settings
+            AudioGraphSettings settings = new AudioGraphSettings(AudioRenderCategory.Media)
+            {
+                //CreateAudioGraphResult result = await AudioGraph.CreateAsync(settings);
+
+                QuantumSizeSelectionMode = QuantumSizeSelectionMode.LowestLatency
+            };
+            var outputDevices = await DeviceInformation.FindAllAsync(MediaDevice.GetAudioRenderSelector());
+
+            foreach (var device in outputDevices)
+            {
+                Debug.WriteLine(device.Name);
+
+                if (device.Name.Contains("Halide"))
+                {
+                    settings.PrimaryRenderDevice = device;
+                }
+            }
+
+            //settings.PrimaryRenderDevice = outputDevices[];
+
+            CreateAudioGraphResult result = await AudioGraph.CreateAsync(settings);
+
+            if (result.Status != AudioGraphCreationStatus.Success)
+            {
+                // Cannot create graph
+                //rootPage.NotifyUser(String.Format("AudioGraph Creation Error because {0}", result.Status.ToString()), NotifyType.ErrorMessage);
+                return;
+            }
+
+            graph = result.Graph;
+
+            // Create a device output node
+            CreateAudioDeviceOutputNodeResult deviceOutputNodeResult = await graph.CreateDeviceOutputNodeAsync();
+            if (deviceOutputNodeResult.Status != AudioDeviceNodeCreationStatus.Success)
+            {
+                // Cannot create device output node
+                //rootPage.NotifyUser(String.Format("Device Output unavailable because {0}", deviceOutputNodeResult.Status.ToString()), NotifyType.ErrorMessage);
+                //speakerContainer.Background = new SolidColorBrush(Colors.Red);
+                return;
+            }
+
+            deviceOutput = deviceOutputNodeResult.DeviceOutputNode;
+            //rootPage.NotifyUser("Device Output Node successfully created", NotifyType.StatusMessage);
+            //speakerContainer.Background = new SolidColorBrush(Colors.Green);
+
         }
     }
 }
---
+++ StreamMachineBA/StartupTask.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Windows.Storage.Streams;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Diagnostics;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using System.Threading.Tasks;
+using Windows.Networking.Sockets;
+using Windows.Devices.Enumeration;
+using Windows.Media.Devices;
+using System.Net;
+using Windows.Networking;
+using System.Threading;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace StreamMachineBA
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        private MediaPlayer mediaPlayer;
+        private Timer timer;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // 
+            // TODO: Insert code to perform background work
+            //
+            // If you start any asynchronous methods here, prevent the task
+            // from closing prematurely by using BackgroundTaskDeferral as
+            // described in http://aka.ms/backgroundtaskdeferral
+            //
+
+            //
+            // Create the deferral by requesting it from the task instance.
+            //
+            BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
+
+            //
+            // Call asynchronous method(s) using the await keyword.
+            //
+            //var result = await ExampleMethodAsync();
+
+            await CreateMediaPlayer();
+
+            var listener = new StreamSocketListener();
+            await listener.BindServiceNameAsync("8000");
+            listener.ConnectionReceived += Listener_ConnectionReceived;
+
+            timer = new Timer(SendStatusBeacon, null, TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(10));
+
+            //
+            // Once the asynchronous method(s) are done, close the deferral.
+            //
+            //deferral.Complete();
+        }
+
+        private async void SendStatusBeacon(object status)
+        {
+            var ds = new DatagramSocket();
+
+            using (var opS = await ds.GetOutputStreamAsync(new HostName("255.255.255.255"), "8377"))
+            {
+                var myIp = await GetCurrentHostName();
+
+                if (myIp != null)
+                {
+                    await opS.WriteAsync(Encoding.UTF8.GetBytes(myIp.ToString()).AsBuffer());
+                    Debug.WriteLine(myIp.ToString());
+                }
+                await opS.FlushAsync();
+            }
+        }
+
+        private async Task<IPAddress> GetCurrentHostName()
+        {
+            if(!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                return null;
+            }
+
+            var hosts = await Dns.GetHostEntryAsync(Dns.GetHostName());
+
+            return hosts.AddressList.FirstOrDefault(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+        }
+
+        private async Task CreateMediaPlayer()
+        {
+            mediaPlayer = new MediaPlayer();
+
+            mediaPlayer.AudioCategory = MediaPlayerAudioCategory.Media;
+
+            var outputDevices = await DeviceInformation.FindAllAsync(MediaDevice.GetAudioRenderSelector());
+
+            foreach (var device in outputDevices)
+            {
+                Debug.WriteLine(device.Name);
+
+                if (device.Name.Contains("Halide"))
+                {
+                    mediaPlayer.AudioDevice = device;
+                }
+            }
+        }
+
+        private void PlayMusic()
+        {
+            //var file = await StorageFile.GetFileFromPathAsync(fil[0].Path);
+
+            //mediaPlayer.Source = MediaSource.CreateFromStorageFile(file);
+
+            var uri = new Uri(@"http://http-live.sr.se/p2musik-mp3-192");
+            //var uri = new Uri(@"http://sverigesradio.se/topsy/direkt/2562-hi-mp3.m3u");
+
+            mediaPlayer.Source = MediaSource.CreateFromUri(uri);
+
+            mediaPlayer.Play();
+        }
+
+        private void StopMusic()
+        {
+            mediaPlayer.Pause();
+            mediaPlayer.Source = null;
+        }
+
+        private async void Listener_ConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            StringBuilder request = new StringBuilder();
+
+            using (IInputStream input = args.Socket.InputStream)
+            {
+                byte[] data = new byte[8192];
+                IBuffer buffer = data.AsBuffer();
+                uint dataRead = 8192;
+                while (dataRead == 8192)
+                {
+                    await input.ReadAsync(buffer, 8192, InputStreamOptions.Partial);
+                    request.Append(Encoding.UTF8.GetString(data, 0, data.Length));
+                    dataRead = buffer.Length;
+                }
+            }
+
+            var req = request.ToString();
+
+            if (req.ToLower().Contains("play"))
+            {
+                PlayMusic();
+            }
+            else if (req.ToLower().Contains("stop"))
+            {
+                StopMusic();
+            }
+
+            Debug.WriteLine(request.ToString());
+
+            string header = string.Format("HTTP/1.1 200 OK\r\n" +
+                              "Content-Type: text/html\r\n" +
+                              "Date: " + DateTime.Now.ToString("R") + "\r\n" +
+                              "Server: IotPlayMusic/1.0\r\n" +
+                              "Content-Length: 2\r\n" +
+                              "Connection: close\r\n\r\n" +
+                              "OK");
+
+            Debug.WriteLine(header);
+            //var buf = Encoding.UTF8.GetBytes("OK").AsBuffer();
+
+            var buf = Encoding.UTF8.GetBytes(header).AsBuffer();
+
+            using (IOutputStream output = args.Socket.OutputStream)
+            {
+                await output.WriteAsync(buf);
+
+                await output.FlushAsync();
+            }
+
+        }
+    }
+}
